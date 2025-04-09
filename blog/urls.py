2cd96c27a747cdc1e6de373c5092bd101b7a752c
Dimitry <tizya.noonie@gmail.com>
2025-04-09 19:59:04 +0300

    Finished step 2.6 of the course on stepik.org

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('index/<int:id>/', views.index),
-    path('access/<int:age>/', views.access),
+    path('', views.index),
+    path('about/', views.about),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
+from django.shortcuts import render
 
 
-def index(request, id):
-    people = [None, 'Bob', 'Sam', 'Tom']
-    # если пользователь найден, возвращаем его
-    if id in range(1, len(people)):
-        return HttpResponse(people[id])
-    # если нет, то возвращаем ошибку 404
-    else:
-        return HttpResponseNotFound('Not Found')
+def index(request):
+    return render(request, 'blog/index.html', context={'site': 'Stepik.org'})
 
 
-def access(request, age):
-    # если возраст НЕ входит в диапазон 1-110, посылаем ошибку 400
-    if age not in range(1, 111):
-        return HttpResponseBadRequest('Некорректные данные')
-    # если возраст меньше 18, то возвращаем ошибку 403
-    if age < 18:
-        return HttpResponseForbidden('Доступ заблокирован: недостаточно лет')
-    # если нет, тогда доступ разрешен
-    else:
-        return HttpResponse('Доступ разрешен')
+def about(request):
+    return render(request, 'blog/about.html', context={'site': 'Stepik'})
